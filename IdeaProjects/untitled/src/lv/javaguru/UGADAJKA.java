89c69a31f564409e660430f2ad68061ef1b9d308
Jurijs Jakunins <jakunins@inbox.lv>
2017-10-27 10:57:45 +0300

    Task solved

--- IdeaProjects/untitled/src/lv/javaguru/UGADAJKA.java
+++ IdeaProjects/untitled/src/lv/javaguru/UGADAJKA.java
@@ -1,4 +1,26 @@
 package lv.javaguru;
+import java.util.*;
 
 public class UGADAJKA {
+    public static void main (String[] args) {
+        Random rand = new Random();
+        int a = rand.nextInt(100);
+        System.out.println("Компьютер загадал число");
+        System.out.println("Отгадайте - введите ваше число");
+        Scanner scanner = new Scanner(System.in);
+        int b = scanner.nextInt();
+
+        while (a!=b) {
+            if (a<b) {
+                System.out.println("Меньше - Ваше число меньше ");
+                b = scanner.nextInt();
+            }
+            if (a>b) {
+                System.out.println("Больше - Ваше число больше ");
+                b = scanner.nextInt();
+            }
+        }
+        System.out.println("Вы угадали");
+    }
+
 }
--- IdeaProjects/untitled/src/lv/javaguru/ZAGADAJKA.java
+++ IdeaProjects/untitled/src/lv/javaguru/ZAGADAJKA.java
@@ -1,4 +1,29 @@
 package lv.javaguru;
+import java.util.*;
 
 public class ZAGADAJKA {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Загадайте ваше число от 0 до 10000");
+        int a = scanner.nextInt();
+
+        System.out.println("Компьютер отгадывает ваше число - подождите:");
+
+        int x = 0;
+        int y = 10000;
+
+        while ((x+y)/2 != a) {
+            if ((x+y)/2 > a) {
+                y = y - (y-x)/2;
+                System.out.print("Число лежит между ");
+                System.out.printf("x=%d; y=%d \n", x, y);
+            }
+            if ((x+y)/2 < a) {
+                x = x + (y-x)/2;
+                System.out.print("Число лежит между ");
+                System.out.printf("x=%d; y=%d \n", x, y);
+            }
+        }
+        System.out.println("Компьютер отгадал: ваше число " + (x+y)/2);
+    }
 }
